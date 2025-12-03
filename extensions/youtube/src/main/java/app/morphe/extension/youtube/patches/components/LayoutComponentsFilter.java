662facb387c3a1f9ef1ab5e9ef5c4c2da8a58838
inotia00 <108592928+inotia00@users.noreply.github.com>
2025-12-03 11:23:44 +0900

    fix(YouTube - Hide layout components): Entire channel header is hidden when the `Visit Community` button or `Visit Store` button is hidden

--- extensions/youtube/src/main/java/app/morphe/extension/youtube/patches/components/LayoutComponentsFilter.java
+++ extensions/youtube/src/main/java/app/morphe/extension/youtube/patches/components/LayoutComponentsFilter.java
@@ -49,7 +49,7 @@ public final class LayoutComponentsFilter extends Filter {
     private final ByteArrayFilterGroup ticketShelf;
     private final StringFilterGroup chipBar;
     private final StringFilterGroup channelProfile;
-    private final ByteArrayFilterGroupList channelProfileBuffer;
+    private final StringFilterGroupList channelProfileGroupList;
     private final ByteArrayFilterGroup playablesBuffer;
 
     public LayoutComponentsFilter() {
@@ -259,12 +259,12 @@ public final class LayoutComponentsFilter extends Filter {
                 "channel_profile.e",
                 "page_header.e"
         );
-        channelProfileBuffer = new ByteArrayFilterGroupList();
-        channelProfileBuffer.addAll(new ByteArrayFilterGroup(
+        channelProfileGroupList = new StringFilterGroupList();
+        channelProfileGroupList.addAll(new StringFilterGroup(
                         Settings.HIDE_VISIT_STORE_BUTTON,
                         "header_store_button"
                 ),
-                new ByteArrayFilterGroup(
+                new StringFilterGroup(
                         Settings.HIDE_VISIT_COMMUNITY_BUTTON,
                         "community_button"
                 )
@@ -335,7 +335,7 @@ public final class LayoutComponentsFilter extends Filter {
         }
 
         if (matchedGroup == channelProfile) {
-            return channelProfileBuffer.check(buffer).isFiltered();
+            return channelProfileGroupList.check(accessibility).isFiltered();
         }
 
         if (matchedGroup == communityPosts && NavigationBar.isBackButtonVisible()) {
