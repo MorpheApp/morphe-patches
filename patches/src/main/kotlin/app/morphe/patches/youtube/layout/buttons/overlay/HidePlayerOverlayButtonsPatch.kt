80ec45656e8824369b4098286b9060978fa01b27
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-12-03 13:51:14 +0200

    chore: Change 20.31.40 to 20.31.42

--- patches/src/main/kotlin/app/morphe/patches/youtube/layout/buttons/overlay/HidePlayerOverlayButtonsPatch.kt
+++ patches/src/main/kotlin/app/morphe/patches/youtube/layout/buttons/overlay/HidePlayerOverlayButtonsPatch.kt
@@ -48,7 +48,7 @@ val hidePlayerOverlayButtonsPatch = bytecodePatch(
             "19.43.41",
             "20.14.43",
             "20.21.37",
-            "20.31.40",
+            "20.31.42",
             "20.46.41",
         )
     )
--- patches/src/main/kotlin/app/morphe/patches/youtube/layout/panels/popup/PlayerPopupPanelsPatch.kt
+++ patches/src/main/kotlin/app/morphe/patches/youtube/layout/panels/popup/PlayerPopupPanelsPatch.kt
@@ -26,7 +26,7 @@ val playerPopupPanelsPatch = bytecodePatch(
             "19.43.41",
             "20.14.43",
             "20.21.37",
-            "20.31.40",
+            "20.31.42",
             "20.46.41",
         )
     )
