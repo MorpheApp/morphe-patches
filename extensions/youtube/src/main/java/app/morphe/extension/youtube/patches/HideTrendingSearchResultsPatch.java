1ab2dacb94edb63f475a1bfa2800bf69084b139b
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2026-01-02 20:43:51 +0100

    refactor: Add setting and strings

--- extensions/youtube/src/main/java/app/morphe/extension/youtube/patches/HideTrendingSearchResultsPatch.java
+++ extensions/youtube/src/main/java/app/morphe/extension/youtube/patches/HideTrendingSearchResultsPatch.java
@@ -1,12 +1,14 @@
 package app.morphe.extension.youtube.patches;
 
+import app.morphe.extension.youtube.settings.Settings;
+
 @SuppressWarnings("unused")
 public class HideTrendingSearchResultsPatch {
 
     /**
      * Injection point.
      */
-    public static boolean isTypingStringEmpty(String value) {
-        return value.isEmpty();
+    public static boolean hideTrendingSearchResult(String typingString) {
+        return Settings.HIDE_TRENDING_SEARCH_RESULTS.get() && typingString.isEmpty();
     }
 }
