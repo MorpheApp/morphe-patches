4c1778133f10d3014102977b6890414ca03146e1
0xrxL <127248639+0xrxL@users.noreply.github.com>
2026-01-09 11:48:56 +0100

    fix (YouTube - Auto-captions): Auto-captions disabler doesn't work for version lower then 20.21

--- extensions/youtube/src/main/java/app/morphe/extension/youtube/patches/AutoCaptionsPatch.java
+++ extensions/youtube/src/main/java/app/morphe/extension/youtube/patches/AutoCaptionsPatch.java
@@ -1,6 +1,6 @@
 package app.morphe.extension.youtube.patches;
-
 import app.morphe.extension.youtube.settings.Settings;
+import app.morphe.patches.youtube.misc.playservice.is_20_26_or_greater
 
 @SuppressWarnings("unused")
 public class AutoCaptionsPatch {
@@ -11,6 +11,10 @@ public class AutoCaptionsPatch {
         WITH_VOLUME_ONLY,
         WITHOUT_VOLUME_ONLY
     }
+    public enum AutoCaptionsStyleLegacy {
+        ENABLED,
+        DISABLED
+    }
 
     private static volatile boolean captionsButtonStatus;
 
@@ -20,8 +24,15 @@ public class AutoCaptionsPatch {
     public static boolean disableAutoCaptions() {
         AutoCaptionsStyle style =
             Settings.AUTO_CAPTIONS_STYLE.get();
-        boolean withVolumeAutoCaptioningEnabled =
-            style == AutoCaptionsStyle.BOTH_ENABLED || style == AutoCaptionsStyle.WITH_VOLUME_ONLY;
+        boolean withVolumeAutoCaptioningEnabled;
+
+        if (is_20_26_or_greater) {
+            withVolumeAutoCaptioningEnabled =
+                style == AutoCaptionsStyle.BOTH_ENABLED || style == AutoCaptionsStyle.WITH_VOLUME_ONLY;
+        } else {
+            withVolumeAutoCaptioningEnabled =
+                style == AutoCaptionsStyleLegacy.ENABLED;
+        }
 
         if (!withVolumeAutoCaptioningEnabled) {
             /**
