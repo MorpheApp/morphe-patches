13b5edc0586c4f0b9aa1cae23c3185251e2b997e
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2026-01-26 21:27:22 +0100

    format indentation, no functional changes

--- patches/src/main/kotlin/app/morphe/patches/reddit/layout/navigation/NavigationButtonsPatch.kt
+++ patches/src/main/kotlin/app/morphe/patches/reddit/layout/navigation/NavigationButtonsPatch.kt
@@ -14,7 +14,8 @@ import com.android.tools.smali.dexlib2.iface.instruction.FiveRegisterInstruction
 import com.android.tools.smali.dexlib2.iface.instruction.ReferenceInstruction
 import com.android.tools.smali.dexlib2.iface.reference.MethodReference
 
-private const val EXTENSION_CLASS_DESCRIPTOR = "Lapp/morphe/extension/reddit/patches/NavigationButtonsPatch;"
+private const val EXTENSION_CLASS_DESCRIPTOR =
+    "Lapp/morphe/extension/reddit/patches/NavigationButtonsPatch;"
 
 @Suppress("unused")
 val navigationButtonsPatch = bytecodePatch(
@@ -27,84 +28,85 @@ val navigationButtonsPatch = bytecodePatch(
 
     execute {
         bottomNavScreenFingerprint.method.apply {
-                implementation!!.instructions
-                    .withIndex()
-                    .filter { (_, instruction) ->
-                        val reference =
-                            (instruction as? ReferenceInstruction)?.reference
-                        instruction.opcode == Opcode.INVOKE_INTERFACE &&
-                                reference is MethodReference &&
-                                reference.toString() == "Ljava/util/List;->add(Ljava/lang/Object;)Z"
-                    }
-                    .map { (index, _) -> index }
-                    .reversed()
-                    .forEach { index ->
-                        val instruction = getInstruction<FiveRegisterInstruction>(index)
-
-                        val listRegister = instruction.registerC
-                        val objectRegister = instruction.registerD
-
-                        replaceInstruction(
-                            index,
-                            "invoke-static { v$listRegister, v$objectRegister }, " +
-                                    "$EXTENSION_CLASS_DESCRIPTOR->" +
-                                    "hideNavigationButtons(Ljava/util/List;Ljava/lang/Object;)V"
-                        )
-                    }
-
-                implementation!!.instructions
-                    .withIndex()
-                    .filter { (_, instruction) ->
-                        val reference = (instruction as? ReferenceInstruction)?.reference
-                        instruction.opcode == Opcode.INVOKE_DIRECT &&
-                                reference is MethodReference &&
-                                reference.definingClass.startsWith("Lcom/reddit/widget/bottomnav/") &&
-                                reference.name == "<init>" &&
-                                reference.parameterTypes.firstOrNull() == "Ljava/lang/String;"
-                    }
-                    .map { (index, _) -> index }
-                    .reversed()
-                    .forEach { index ->
-                        val instruction = getInstruction<FiveRegisterInstruction>(index)
-
-                        val objectRegister = instruction.registerC
-                        val labelRegister = instruction.registerD
-
-                        addInstruction(
-                            index + 1,
-                            "invoke-static { v$objectRegister, v$labelRegister }, " +
-                                    "$EXTENSION_CLASS_DESCRIPTOR->" +
-                                    "setNavigationMap(Ljava/lang/Object;Ljava/lang/String;)V"
-                        )
-                    }
-
-                val getResStringFilter = methodCall(
-                    smali = "Landroid/content/res/Resources;->getString(I)Ljava/lang/String;",
-                    opcode = Opcode.INVOKE_VIRTUAL
-                )
-                val matchingInstructions = instructions.mapIndexedNotNull { index, instruction ->
-                    if (getResStringFilter.matches(this, instruction)) {
-                        return@mapIndexedNotNull index to instruction
-                    }
-                    return@mapIndexedNotNull null
+            implementation!!.instructions
+                .withIndex()
+                .filter { (_, instruction) ->
+                    val reference =
+                        (instruction as? ReferenceInstruction)?.reference
+                    instruction.opcode == Opcode.INVOKE_INTERFACE &&
+                            reference is MethodReference &&
+                            reference.toString() == "Ljava/util/List;->add(Ljava/lang/Object;)Z"
                 }
+                .map { (index, _) -> index }
+                .reversed()
+                .forEach { index ->
+                    val instruction = getInstruction<FiveRegisterInstruction>(index)
 
-                matchingInstructions.reversed().forEach { value ->
-                    val (index, instruction) = value
-                    val idReg = (instruction as FiveRegisterInstruction).registerD
-                    addInstruction(
+                    val listRegister = instruction.registerC
+                    val objectRegister = instruction.registerD
+
+                    replaceInstruction(
                         index,
-                        "invoke-static { v$idReg }, " +
-                                "$EXTENSION_CLASS_DESCRIPTOR->mapResourceId(I)V"
+                        "invoke-static { v$listRegister, v$objectRegister }, " +
+                                "$EXTENSION_CLASS_DESCRIPTOR->" +
+                                "hideNavigationButtons(Ljava/util/List;Ljava/lang/Object;)V"
                     )
                 }
 
+            implementation!!.instructions
+                .withIndex()
+                .filter { (_, instruction) ->
+                    val reference = (instruction as? ReferenceInstruction)?.reference
+                    instruction.opcode == Opcode.INVOKE_DIRECT &&
+                            reference is MethodReference &&
+                            reference.definingClass.startsWith("Lcom/reddit/widget/bottomnav/") &&
+                            reference.name == "<init>" &&
+                            reference.parameterTypes.firstOrNull() == "Ljava/lang/String;"
+                }
+                .map { (index, _) -> index }
+                .reversed()
+                .forEach { index ->
+                    val instruction = getInstruction<FiveRegisterInstruction>(index)
+
+                    val objectRegister = instruction.registerC
+                    val labelRegister = instruction.registerD
+
+                    addInstruction(
+                        index + 1,
+                        "invoke-static { v$objectRegister, v$labelRegister }, " +
+                                "$EXTENSION_CLASS_DESCRIPTOR->" +
+                                "setNavigationMap(Ljava/lang/Object;Ljava/lang/String;)V"
+                    )
+                }
+
+            val getResStringFilter = methodCall(
+                opcode = Opcode.INVOKE_VIRTUAL,
+                smali = "Landroid/content/res/Resources;->getString(I)Ljava/lang/String;"
+            )
+
+            val matchingInstructions = instructions.mapIndexedNotNull { index, instruction ->
+                if (getResStringFilter.matches(this, instruction)) {
+                    return@mapIndexedNotNull index to instruction
+                }
+                return@mapIndexedNotNull null
+            }
+
+            matchingInstructions.reversed().forEach { value ->
+                val (index, instruction) = value
+                val idReg = (instruction as FiveRegisterInstruction).registerD
                 addInstruction(
-                    0,
-                    "invoke-static/range { p1 .. p1 }, " +
-                            "$EXTENSION_CLASS_DESCRIPTOR->setResources(Landroid/content/res/Resources;)V"
+                    index,
+                    "invoke-static { v$idReg }, " +
+                            "$EXTENSION_CLASS_DESCRIPTOR->mapResourceId(I)V"
                 )
             }
+
+            addInstruction(
+                0,
+                "invoke-static/range { p1 .. p1 }, " +
+                        "$EXTENSION_CLASS_DESCRIPTOR->setResources(Landroid/content/res/Resources;)V"
+            )
+        }
 
         setExtensionIsPatchIncluded(EXTENSION_CLASS_DESCRIPTOR)
     }
