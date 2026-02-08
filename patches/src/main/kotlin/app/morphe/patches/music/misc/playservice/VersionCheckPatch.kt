44740e7797dc6f3854da09f150dd89a5a62f3fb0
inotia00 <108592928+inotia00@users.noreply.github.com>
2026-02-08 18:11:07 +0900

    refactor: Instead of comparing `google_play_services_version`, the version name is used

--- patches/src/main/kotlin/app/morphe/patches/music/misc/playservice/VersionCheckPatch.kt
+++ patches/src/main/kotlin/app/morphe/patches/music/misc/playservice/VersionCheckPatch.kt
@@ -3,7 +3,8 @@
 package app.morphe.patches.music.misc.playservice
 
 import app.morphe.patcher.patch.resourcePatch
-import app.morphe.util.findPlayStoreServicesVersion
+import app.morphe.patches.shared.misc.metadata.isGreaterThan
+import app.morphe.patches.shared.misc.metadata.packageMetaDataPatch
 import kotlin.properties.Delegates
 
 // Use notNull delegate so an exception is thrown if these fields are accessed before they are set.
@@ -26,21 +27,18 @@ var is_8_41_or_greater: Boolean by Delegates.notNull()
     private set
 
 val versionCheckPatch = resourcePatch(
-    description = "Uses the Play Store service version to find the major/minor version of the YouTube Music target app.",
+    description = "Find the major/minor version of the YouTube Music target app.",
 ) {
-    execute {
-        // The app version is missing from the decompiled manifest,
-        // so instead use the Google Play services version and compare against specific releases.
-        val playStoreServicesVersion = findPlayStoreServicesVersion()
+    dependsOn(packageMetaDataPatch)
 
-        // All bug fix releases always seem to use the same play store version as the minor version.
-        is_7_16_or_greater = 243499000 <= playStoreServicesVersion
-        is_7_33_or_greater = 245199000 <= playStoreServicesVersion
-        is_8_05_or_greater = 250599000 <= playStoreServicesVersion
-        is_8_10_or_greater = 251099000 <= playStoreServicesVersion
-        is_8_11_or_greater = 251199000 <= playStoreServicesVersion
-        is_8_15_or_greater = 251530000 <= playStoreServicesVersion
-        is_8_40_or_greater = 254080000 <= playStoreServicesVersion
-        is_8_41_or_greater = 254180000 <= playStoreServicesVersion
+    execute {
+        is_7_16_or_greater = isGreaterThan(716)
+        is_7_33_or_greater = isGreaterThan(733)
+        is_8_05_or_greater = isGreaterThan(805)
+        is_8_10_or_greater = isGreaterThan(810)
+        is_8_11_or_greater = isGreaterThan(811)
+        is_8_15_or_greater = isGreaterThan(815)
+        is_8_40_or_greater = isGreaterThan(840)
+        is_8_41_or_greater = isGreaterThan(841)
     }
 }
