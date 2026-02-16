1afbffb2e9b087fd25adc8f8e93b6e21be4730ef
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2026-02-16 13:19:27 +0100

    Add 2026.04.0

--- patches/src/main/kotlin/app/morphe/patches/reddit/shared/Constants.kt
+++ patches/src/main/kotlin/app/morphe/patches/reddit/shared/Constants.kt
@@ -11,6 +11,7 @@ internal object Constants {
     val COMPATIBILITY_REDDIT: Pair<PackageName, Set<VersionName>> = Pair(
         "com.reddit.frontpage",
         setOf(
+            "2026.04.0",
             "2026.03.0",
             "2025.48.0",
         )
