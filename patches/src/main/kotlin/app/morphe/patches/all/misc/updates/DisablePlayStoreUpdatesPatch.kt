84218343d58dc8bd0924c839e908f84fdd8d2ce9
Eric Ahn <wchill1337@gmail.com>
2026-02-18 12:09:14 -0800

    Apply review feedback

---
+++ patches/src/main/kotlin/app/morphe/patches/all/misc/updates/DisablePlayStoreUpdatesPatch.kt
@@ -0,0 +1,21 @@
+package app.morphe.patches.all.misc.updates
+
+import app.morphe.patcher.patch.resourcePatch
+import app.morphe.util.getNode
+import org.w3c.dom.Element
+
+internal val disablePlayStoreUpdatesPatch = resourcePatch(
+    name = "Disable Play Store updates",
+    description = "Disables Play Store updates by setting the version code to the maximum allowed. " +
+            "This can have unexpected issues in some apps.",
+    use = false
+) {
+    finalize {
+        document("AndroidManifest.xml").use { document ->
+            val manifest = document.getNode("manifest") as Element
+
+            // set version code to max allowed by Play Store
+            manifest.setAttribute("android:versionCode", "2100000000")
+        }
+    }
+}
--- patches/src/main/kotlin/app/morphe/patches/reddit/misc/settings/SettingsPatch.kt
+++ patches/src/main/kotlin/app/morphe/patches/reddit/misc/settings/SettingsPatch.kt
@@ -8,7 +8,7 @@ import app.morphe.patcher.extensions.InstructionExtensions.addInstructions
 import app.morphe.patcher.extensions.InstructionExtensions.addInstructionsWithLabels
 import app.morphe.patcher.extensions.InstructionExtensions.getInstruction
 import app.morphe.patcher.patch.bytecodePatch
-import app.morphe.patches.all.misc.versioncode.changeVersionCodePatch
+import app.morphe.patches.all.misc.updates.changeVersionCodePatch
 import app.morphe.patches.reddit.misc.extension.hooks.redditActivityOnCreateHook
 import app.morphe.patches.reddit.misc.extension.sharedExtensionPatch
 import app.morphe.patches.reddit.shared.Constants.COMPATIBILITY_REDDIT
