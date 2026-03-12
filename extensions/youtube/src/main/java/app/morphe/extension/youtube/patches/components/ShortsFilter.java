b2a6b4a13b42ef02232823c5ff29aa0ed00b4fd1
ILoveOpenSouceApplications <em29072chinmayjagalchandrababu@gmail.com>
2026-03-12 22:59:02 +0530

    refactor: Use correct condition

--- extensions/youtube/src/main/java/app/morphe/extension/youtube/patches/components/ShortsFilter.java
+++ extensions/youtube/src/main/java/app/morphe/extension/youtube/patches/components/ShortsFilter.java
@@ -458,8 +458,8 @@ public final class ShortsFilter extends Filter {
             }
 
             if (matchedGroup == shortsActionButton) {
-                return path.startsWith(REEL_PLAYER_OVERLAY_PATH) || path.startsWith(REEL_METAPANEL_PATH)
-                && useButtonsBuffer.check(buffer).isFiltered();
+                if (path.startsWith(REEL_PLAYER_OVERLAY_PATH) || path.startsWith(REEL_METAPANEL_PATH))
+                    return useButtonsBuffer.check(buffer).isFiltered();
             }
 
             if (matchedGroup == suggestedAction) {
