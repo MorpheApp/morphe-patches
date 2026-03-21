053d678332aec6b2f981853e0cc60c50ac9db5cd
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2026-03-21 11:39:06 +0100

    Cleanup temporary changes, use 'not a hard fork' comment because it's 100% new code

--- patches/src/main/kotlin/app/morphe/patches/youtube/shared/Constants.kt
+++ patches/src/main/kotlin/app/morphe/patches/youtube/shared/Constants.kt
@@ -7,7 +7,6 @@ internal object Constants {
     val COMPATIBILITY_YOUTUBE: Pair<PackageName, Set<VersionName>> = Pair(
         "com.google.android.youtube",
         setOf(
-            "21.12.522",
             "20.45.36",
             "20.44.38",
             "20.40.45",
