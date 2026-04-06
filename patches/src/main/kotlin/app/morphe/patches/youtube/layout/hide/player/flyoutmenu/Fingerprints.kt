d09a51e0689f02df2e6d02db0c04e369e27e531f
ILoveOpenSouceApplications <em29072chinmayjagalchandrababu@gmail.com>
2026-04-07 02:35:08 +0530

    chore: Remove unused `Fingerprints.kt` file

--- patches/src/main/kotlin/app/morphe/patches/youtube/layout/hide/player/flyoutmenu/Fingerprints.kt
+++
@@ -1,9 +0,0 @@
-package app.morphe.patches.youtube.layout.hide.player.flyoutmenu
-
-import app.morphe.patcher.Fingerprint
-
-internal object RecyclerViewOnAttachedToWindowFingerprint : Fingerprint(
-    definingClass = "Landroidx/recyclerview/widget/RecyclerView;",
-    name = "onAttachedToWindow",
-    returnType = "V"
-)
