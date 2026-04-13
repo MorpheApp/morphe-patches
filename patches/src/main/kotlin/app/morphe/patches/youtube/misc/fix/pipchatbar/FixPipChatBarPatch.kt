09081d1e5c9a32abc2a64767b0e320632c46a595
FlaminSarge <flaminsarge@gmail.com>
2026-04-13 14:27:39 -0700

    Make patch always-on

--- patches/src/main/kotlin/app/morphe/patches/youtube/misc/fix/pipchatbar/FixPipChatBarPatch.kt
+++ patches/src/main/kotlin/app/morphe/patches/youtube/misc/fix/pipchatbar/FixPipChatBarPatch.kt
@@ -3,19 +3,15 @@ package app.morphe.patches.youtube.misc.fix.pipchatbar
 import app.morphe.patcher.extensions.InstructionExtensions.addInstruction
 import app.morphe.patcher.patch.bytecodePatch
 import app.morphe.patches.youtube.misc.extension.sharedExtensionPatch
-import app.morphe.patches.youtube.shared.Constants.COMPATIBILITY_YOUTUBE
 
 private const val EXTENSION_CLASS_DESCRIPTOR =
     "Lapp/morphe/extension/youtube/patches/FixPipChatBarPatch;"
 
-@Suppress("unused")
-val fixPipChatBarPatch = bytecodePatch(
-    name = "Fix PiP chat bar",
-    description = "Hides the bar that appears over the PiP video after using live chat text entry.",
-) {
+/**
+ * Hides the bar that appears over the PiP video after using live chat text entry.
+ */
+internal val fixPipChatBarPatch = bytecodePatch{
     dependsOn(sharedExtensionPatch)
-
-    compatibleWith(COMPATIBILITY_YOUTUBE)
 
     execute {
         PipModeChangedFingerprint.method.addInstruction(
