9cd40420e7cdb21db5aab5c52d646f17f097417c
ILoveOpenSouceApplications <117499019+ILoveOpenSourceApplications@users.noreply.github.com>
2026-06-04 04:35:17 +0530

    revert changes

--- patches/src/main/kotlin/app/morphe/patches/shared/layout/theme/LithoColorHookPatch.kt
+++ patches/src/main/kotlin/app/morphe/patches/shared/layout/theme/LithoColorHookPatch.kt
@@ -11,22 +11,19 @@
 package app.morphe.patches.shared.layout.theme
 
 import app.morphe.patcher.extensions.InstructionExtensions.addInstructions
-import app.morphe.patcher.extensions.InstructionExtensions.getInstruction
 import app.morphe.patcher.patch.bytecodePatch
 import app.morphe.patcher.util.proxy.mutableTypes.MutableMethod
-import com.android.tools.smali.dexlib2.iface.instruction.FiveRegisterInstruction
 import java.lang.ref.WeakReference
 
 private lateinit var lithoColorOverrideHookRef : WeakReference<MutableMethod>
 private var lithoColorOverrideHookInsertIndex = -1
-private var colorRegister = -1
 
 fun lithoColorOverrideHook(targetMethodClass: String, targetMethodName: String) {
     lithoColorOverrideHookRef.get()!!.addInstructions(
         lithoColorOverrideHookInsertIndex,
         """
-            invoke-static { v$colorRegister }, $targetMethodClass->$targetMethodName(I)I
-            move-result v$colorRegister
+            invoke-static { p1 }, $targetMethodClass->$targetMethodName(I)I
+            move-result p1
         """
     )
     lithoColorOverrideHookInsertIndex += 2
@@ -35,14 +32,9 @@ fun lithoColorOverrideHook(targetMethodClass: String, targetMethodName: String) 
 val lithoColorHookPatch = bytecodePatch(
     description = "Adds a hook to set color of Litho components.",
 ) {
+
     execute {
-        val method = LithoOnBoundsChangeFingerprint.method
-        lithoColorOverrideHookRef = WeakReference(method)
-
-        val setColorIndex = LithoOnBoundsChangeFingerprint.instructionMatches.last().index
-        lithoColorOverrideHookInsertIndex = setColorIndex
-
-        val instruction = method.getInstruction<FiveRegisterInstruction>(setColorIndex)
-        colorRegister = instruction.registerD
+        lithoColorOverrideHookRef = WeakReference(LithoOnBoundsChangeFingerprint.method)
+        lithoColorOverrideHookInsertIndex = LithoOnBoundsChangeFingerprint.instructionMatches.last().index - 1
     }
 }
