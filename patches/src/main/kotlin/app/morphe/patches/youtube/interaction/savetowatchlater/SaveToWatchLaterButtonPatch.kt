05a6ea26eb3ddc220d31a74f64da1eae1ed16dfa
MarcaDian <152095496+MarcaDian@users.noreply.github.com>
2026-06-04 08:33:15 +0300

    Add missing settings summary

--- patches/src/main/kotlin/app/morphe/patches/youtube/interaction/savetowatchlater/SaveToWatchLaterButtonPatch.kt
+++ patches/src/main/kotlin/app/morphe/patches/youtube/interaction/savetowatchlater/SaveToWatchLaterButtonPatch.kt
@@ -68,7 +68,7 @@ val saveToWatchLaterButtonPatch = bytecodePatch(
 
     execute {
         addPlayerOverlayPreferences(
-            SwitchPreference("morphe_save_to_watch_later_button")
+            SwitchPreference("morphe_save_to_watch_later_button", summary = true)
         )
 
         initializeTopControl(EXTENSION_BUTTON)
