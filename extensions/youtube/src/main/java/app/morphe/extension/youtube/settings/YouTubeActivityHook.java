94a9751c34b02cfec38e607260e5b861d086ff07
MarcaDian <152095496+MarcaDian@users.noreply.github.com>
2026-06-04 16:13:25 +0300

    Suppress deprecation warning

--- extensions/youtube/src/main/java/app/morphe/extension/youtube/settings/YouTubeActivityHook.java
+++ extensions/youtube/src/main/java/app/morphe/extension/youtube/settings/YouTubeActivityHook.java
@@ -32,6 +32,7 @@ import app.morphe.extension.youtube.settings.search.YouTubeSearchViewController;
  * Hooks {@link GoogleApiActivity} to inject a custom {@link YouTubePreferenceFragment}
  * with a toolbar and search functionality.
  */
+@SuppressWarnings("deprecation")
 public class YouTubeActivityHook extends BaseActivityHook {
 
     /**
