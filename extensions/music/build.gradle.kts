9c9aa337b28b57df9e2d3a36f73bc9a1b1f1a16b
PrathxmOp <rizlagogo@Gmail.com>
2026-06-27 11:52:54 +0530

    fix: remove listen together dependency and dummy credentials

--- extensions/music/build.gradle.kts
+++ extensions/music/build.gradle.kts
@@ -7,9 +7,6 @@ dependencies {
     compileOnly(project(":extensions:youtube:stub"))
     compileOnly(libs.annotation)
     compileOnly(libs.gson)
-    
-    // For Listen Together
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
     compileOnly(libs.protobuf.javalite)
 }
 
