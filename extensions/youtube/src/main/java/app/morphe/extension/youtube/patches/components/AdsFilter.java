f117f10354c7e1a6047b1fff9bb52833e56405db
ILoveOpenSourceApplications <117499019+ILoveOpenSourceApplications@users.noreply.github.com>
2026-07-14 03:21:09 +0530

    fix(YouTube - Hide ads): Hide shopping links in transcripts

--- extensions/youtube/src/main/java/app/morphe/extension/youtube/patches/components/AdsFilter.java
+++ extensions/youtube/src/main/java/app/morphe/extension/youtube/patches/components/AdsFilter.java
@@ -53,11 +53,11 @@ public final class AdsFilter extends Filter {
 
     public AdsFilter() {
         exceptions.addPatterns(
-                "home_video_with_context", // Don't filter anything in the home page video component.
-                "related_video_with_context", // Don't filter anything in the related video component.
-                "comment_thread", // Don't filter anything in the comments.
                 "|comment.", // Don't filter anything in the comments replies.
-                "library_recent_shelf"
+                "comment_thread", // Don't filter anything in the comments.
+                "home_video_with_context", // Don't filter anything in the home page video component.
+                "library_recent_shelf",
+                "related_video_with_context" // Don't filter anything in the related video component.
         );
 
         // Identifiers.
@@ -83,15 +83,16 @@ public final class AdsFilter extends Filter {
                 "compact_landscape_image_layout", // Tablet layout search results.
                 "composite_concurrent_carousel_layout",
                 "full_width_portrait_image_layout",
+                // full_width_square_image_button_group_layout, landscape_image_button_group_layout, text_image_button_group_layout
                 "full_width_square_image_carousel_layout",
                 "full_width_square_image_layout",
                 "hero_promo_image",
-                // text_image_button_group_layout, landscape_image_button_group_layout, full_width_square_image_button_group_layout
                 "image_button_group_layout",
                 "landscape_image_carousel_layout",
                 "landscape_image_wide_button_layout",
                 "primetime_promo",
                 "product_details",
+                "shopping_timely_shelf.", // Injection point below hides the empty space.
                 "square_image_layout",
                 "text_image_button_layout",
                 "text_image_no_button_layout", // Tablet layout search results.
@@ -100,8 +101,7 @@ public final class AdsFilter extends Filter {
                 "video_display_carousel_buttoned_short_dr_layout",
                 "video_display_full_buttoned_short_dr_layout",
                 "video_display_full_layout",
-                "watch_metadata_app_promo",
-                "shopping_timely_shelf." // Injection point below hides the empty space.
+                "watch_metadata_app_promo"
         );
 
         final var movieAds = new StringFilterGroup(
@@ -126,13 +126,14 @@ public final class AdsFilter extends Filter {
 
         final var viewProducts = new StringFilterGroup(
                 Settings.HIDE_PLAYER_POPUP_ADS,
-                "product_item",
                 "products_in_video",
+                "product_item",
                 "shopping_overlay.e" // Video player overlay shopping links.
         );
 
         final var shoppingLinks = new StringFilterGroup(
                 Settings.HIDE_SHOPPING_LINKS,
+                "shopping_description_item.e",
                 "shopping_description_shelf.e"
         );
 
@@ -151,8 +152,8 @@ public final class AdsFilter extends Filter {
 
         final var productSticker = new StringFilterGroup(
                 Settings.HIDE_PLAYER_POPUP_ADS,
-                "stickers_layer.e",
-                "product_sticker.e" // Product sticker that appears on Shorts.
+                "product_sticker.e", // Product sticker that appears on Shorts.
+                "stickers_layer.e"
         );
 
         final var selfSponsor = new StringFilterGroup(
