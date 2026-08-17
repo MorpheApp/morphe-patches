fcf79856bc970009b17797b318aa61aa9863cd8f
inotia00 <108592928+inotia00@users.noreply.github.com>
2026-08-17 13:32:52 +0900

    fix(Spoof video streams): Reduce the visitorData lifespan to 30 days and do not remove it via AuthUtils

--- extensions/shared-youtube/library/src/main/java/app/morphe/extension/shared/spoof/requests/VisitorIdRequester.java
+++ extensions/shared-youtube/library/src/main/java/app/morphe/extension/shared/spoof/requests/VisitorIdRequester.java
@@ -33,7 +33,7 @@ import app.morphe.extension.shared.spoof.ClientType;
 public final class VisitorIdRequester {
 
     private record VisitorData(String visitorId, long fetchedTime) {
-        private static final long VISITOR_ID_EXPIRATION_MS = (long) 365 * 24 * 60 * 60 * 1000; // 1 year
+        private static final long VISITOR_ID_EXPIRATION_MS = 30L * 24 * 60 * 60 * 1000; // 30 days
 
         boolean isNotExpired() {
             return Utils.isNotEmpty(visitorId) && System.currentTimeMillis()
@@ -92,22 +92,10 @@ public final class VisitorIdRequester {
     }
 
     private static void saveVisitorId(ClientType clientType, String visitorId, boolean updatedByPlayer) {
-        Logger.printDebug(() -> "Updating visitorId for clientType: " + clientType + " updated by player: " + updatedByPlayer);
-        updateVisitorId(clientType, visitorId, true);
-    }
+        Logger.printDebug(() -> "Save visitorId for clientType: " + clientType + " updated by player: " + updatedByPlayer);
 
-    public static void removeVisitorId(ClientType clientType) {
-        Logger.printDebug(() -> "Removing visitorId for clientType: " + clientType);
-        updateVisitorId(clientType, "", false);
-    }
-
-    private static void updateVisitorId(ClientType clientType, String visitorId, boolean save) {
         synchronized (cache) {
-            if (save) {
-                cache.put(clientType, new VisitorData(visitorId, System.currentTimeMillis()));
-            } else {
-                cache.remove(clientType);
-            }
+            cache.put(clientType, new VisitorData(visitorId, System.currentTimeMillis()));
             JSONObject json = new JSONObject();
             try {
                 for (Map.Entry<ClientType, VisitorData> entry : cache.entrySet()) {
