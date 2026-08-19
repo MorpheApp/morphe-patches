dd7da253615cb939b3db175ff8a458e33f2b60a1
inotia00 <108592928+inotia00@users.noreply.github.com>
2026-08-19 14:20:32 +0900

    fix(Spoof video streams): Remove Android VR from available clients

--- extensions/music/src/main/java/app/morphe/extension/music/patches/spoof/SpoofVideoStreamsPatch.java
+++ extensions/music/src/main/java/app/morphe/extension/music/patches/spoof/SpoofVideoStreamsPatch.java
@@ -1,11 +1,8 @@
 package app.morphe.extension.music.patches.spoof;
 
-import android.text.TextUtils;
-
 import java.util.List;
 
 import app.morphe.extension.music.settings.Settings;
-import app.morphe.extension.shared.oauth2.requests.OAuth2Requester;
 import app.morphe.extension.shared.spoof.ClientType;
 
 @SuppressWarnings("unused")
@@ -17,19 +14,13 @@ public class SpoofVideoStreamsPatch {
     public static void setClientOrderToUse() {
         List<ClientType> availableClients = List.of(
                 ClientType.TV_SABR,
-                ClientType.ANDROID_VR,
                 ClientType.VISIONOS_1_02,
                 ClientType.ANDROID_MUSIC_NO_SDK,
                 ClientType.ANDROID_MUSIC_REEL
+                // If not signed in to Android VR, there may be playback issues.
+                // Only use it if the user has selected it.
+                // ClientType.ANDROID_VR
         );
-
-        // If not signed in to Android VR, there may be playback issues.
-        // If the user has not signed in to Android VR, remove them from the available clients.
-        // Only use it if the user has selected it.
-        String oauth2Authorization = OAuth2Requester.getAndUpdateAccessTokenIfNeeded();
-        if (TextUtils.isEmpty(oauth2Authorization)) {
-            availableClients.remove(ClientType.ANDROID_VR);
-        }
 
         app.morphe.extension.shared.spoof.SpoofVideoStreamsPatch.setClientsToUse(
                 availableClients, Settings.SPOOF_VIDEO_STREAMS_CLIENT_TYPE.get());
--- extensions/youtube/src/main/java/app/morphe/extension/youtube/patches/spoof/SpoofVideoStreamsPatch.java
+++ extensions/youtube/src/main/java/app/morphe/extension/youtube/patches/spoof/SpoofVideoStreamsPatch.java
@@ -1,10 +1,7 @@
 package app.morphe.extension.youtube.patches.spoof;
 
-import android.text.TextUtils;
-
 import java.util.List;
 
-import app.morphe.extension.shared.oauth2.requests.OAuth2Requester;
 import app.morphe.extension.shared.settings.Setting;
 import app.morphe.extension.shared.spoof.ClientType;
 import app.morphe.extension.youtube.settings.Settings;
@@ -45,18 +42,12 @@ public class SpoofVideoStreamsPatch {
 
         List<ClientType> availableClients = List.of(
                 ClientType.TV_SABR,
-                ClientType.ANDROID_VR,
                 ClientType.VISIONOS_1_02,
                 ClientType.ANDROID_CREATOR
+                // If not signed in to Android VR, there may be playback issues.
+                // Only use it if the user has selected it.
+                // ClientType.ANDROID_VR
         );
-
-        // If not signed in to Android VR, there may be playback issues.
-        // If the user has not signed in to Android VR, remove them from the available clients.
-        // Only use it if the user has selected it.
-        String oauth2Authorization = OAuth2Requester.getAndUpdateAccessTokenIfNeeded();
-        if (TextUtils.isEmpty(oauth2Authorization)) {
-            availableClients.remove(ClientType.ANDROID_VR);
-        }
 
         app.morphe.extension.shared.spoof.SpoofVideoStreamsPatch.setClientsToUse(
                 availableClients, client);
