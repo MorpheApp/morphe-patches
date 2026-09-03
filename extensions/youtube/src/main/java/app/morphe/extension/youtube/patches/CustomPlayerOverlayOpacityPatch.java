92186e34b1c8e64758c8c6c22b920cbc35750701
MarcaDian <152095496+MarcaDian@users.noreply.github.com>
2026-09-03 19:27:48 +0300

    feat(YouTube - Hide player overlay buttons): Add option to change the player control buttons background opacity

--- extensions/youtube/src/main/java/app/morphe/extension/youtube/patches/CustomPlayerOverlayOpacityPatch.java
+++ extensions/youtube/src/main/java/app/morphe/extension/youtube/patches/CustomPlayerOverlayOpacityPatch.java
@@ -1,27 +1,25 @@
-package app.morphe.extension.youtube.patches;
+/*
+ * Copyright 2026 Morphe.
+ * https://github.com/MorpheApp/morphe-patches/pull/2764
+ *
+ * Original hard forked code:
+ * https://github.com/ReVanced/revanced-patches/commit/724e6d61b2ecd868c1a9a37d465a688e83a74799
+ *
+ * See the included NOTICE file for GPLv3 Section 7 terms that apply to Morphe contributions.
+ */
 
-import static app.morphe.extension.shared.StringRef.str;
+package app.morphe.extension.youtube.patches;
 
 import android.widget.ImageView;
 
-import app.morphe.extension.shared.Utils;
+import app.morphe.extension.shared.settings.preference.SeekBarPreference;
 import app.morphe.extension.youtube.settings.Settings;
 
 @SuppressWarnings("unused")
 public class CustomPlayerOverlayOpacityPatch {
 
-    private static final int PLAYER_OVERLAY_OPACITY_LEVEL;
-
-    static {
-        int opacity = Settings.PLAYER_OVERLAY_OPACITY.get();
-
-        if (opacity < 0 || opacity > 100) {
-            Utils.showToastLong(str("morphe_player_overlay_opacity_invalid_toast"));
-            opacity = Settings.PLAYER_OVERLAY_OPACITY.resetToDefault();
-        }
-
-        PLAYER_OVERLAY_OPACITY_LEVEL = (opacity * 255) / 100;
-    }
+    private static final int PLAYER_OVERLAY_OPACITY_LEVEL =
+            (SeekBarPreference.clampToRange(Settings.PLAYER_OVERLAY_OPACITY) * 255) / 100;
 
     /**
      * Injection point.
