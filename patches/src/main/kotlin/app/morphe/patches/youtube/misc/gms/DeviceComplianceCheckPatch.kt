1a125d68d15d57e98fa6c0cd3699c0bb2381392e
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2026-09-15 18:28:19 +0200

    refactor

--- patches/src/main/kotlin/app/morphe/patches/youtube/misc/gms/DeviceComplianceCheckPatch.kt
+++ patches/src/main/kotlin/app/morphe/patches/youtube/misc/gms/DeviceComplianceCheckPatch.kt
@@ -8,21 +8,15 @@
 package app.morphe.patches.youtube.misc.gms
 
 import app.morphe.patcher.extensions.InstructionExtensions.addInstructions
-import app.morphe.patcher.extensions.InstructionExtensions.getInstruction
 import app.morphe.patcher.patch.bytecodePatch
-import app.morphe.util.getReference
-import com.android.tools.smali.dexlib2.iface.reference.MethodReference
 
 internal val deviceComplianceCheckPatch = bytecodePatch {
-
     execute {
         // GmsCore answering "not compliant" sends the app to the background and opens
         // UncertifiedDeviceActivity. A failed check already emits false, so do the same.
-        GmsDeviceComplianceCheckFingerprint.method.apply {
-            val emitterIndex = GmsDeviceComplianceCheckFingerprint.instructionMatches.last().index
-            val emitterReference = getInstruction(emitterIndex).getReference<MethodReference>()!!
-
-            addInstructions(
+        GmsDeviceComplianceCheckFingerprint.let {
+            val emitterReference = it.instructionMatches.last().getMethodCalled()
+            it.method.addInstructions(
                 0,
                 """
                     sget-object v0, Ljava/lang/Boolean;->FALSE:Ljava/lang/Boolean;
